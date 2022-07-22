c5363591d87073a8bdc0a4d3bf786d35c3273787
Luisibz97 <luis_rab_20@hotmail.com>
2022-07-22 19:52:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+const error = () => {
+    throw new Error('Siempre da error esta función')
+}
+
+
+try {
+    error()
+} catch (e) {
+    logger.error('La función dio error por eso se ejecuta este mensaje') 
+}
